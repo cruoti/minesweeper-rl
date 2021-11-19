951b44c25ad9441ab035f294029c015692583a27
Christopher Ruoti <cruoti@gmail.com>
2021-11-19 15:48:28 -0700

    create openAI gym environment

--- minesweeper/minesweeper-env.py
+++ minesweeper/minesweeper-env.py
@@ -1,6 +1,12 @@
 import random
+from time import sleep
 import gym
 from gym import spaces
+import pygame
+
+from backend import EasyGame, MedGame, HardGame
+from frontend import SPRITE_UNKNWN
+from frontend import ScreenCell, ScreenField
 
 
 class MinesweeperEnv(gym.Env):
@@ -13,87 +19,105 @@ class MinesweeperEnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=10, shape=(self.game.rows, self.game.cols), dtype=int)
 
         # not required
+        self.memory = set([])
 
+    @property
+    def known_grid(self):
+        grid = self.game.create_empty_field(None, self.game.rows, self.game.cols)
+        for i in range(self.game.rows):
+            for j in range(self.game.cols):
+                if self.game.mine_field[i][j].is_known:
+                    grid[i][j] = self.game.mine_field[i][j].value
+                else:
+                    grid[i][j] = -2
+        return grid
 
     def step(self, action):
-        i = random.randrange(0, self.game.rows)
-        j = random.randrange(0, self.game.cols)
+        i = action // self.game.cols
+        j = action % self.game.cols 
+
+        # i = random.randrange(0, self.game.rows)
+        # j = random.randrange(0, self.game.cols)
         
-        self.game.reveal(i, j)
-        state = self.game.known_grid
-
-        reward = 0
-        if i == 0:
-            reward = 1
-            if j == 2:
-                reward += 2
-            
-        if i == 3 and j == 1:
-            done = True
-        else:
+        if (i, j) in self.memory:
+            reward = -5
             done = False
+            info = {}
+            print(f'ALREADY CLICKED: {reward}')    
+            return self.known_grid, reward, done, info
+        self.memory.add((i, j))
 
-        info = {}
-        return state, reward, done, info
+        is_game_over = self.game.click_cell(i, j)
+
+        if not is_game_over:
+            reward = 1
+            done = False
+            info = {}
+            print(f'GOOD CLICK: {reward}')
+            return self.known_grid, reward, done, info
+        else:
+            done = True
+            info = {}
+            if self.game.is_won:
+                reward = 1
+                print(f'WON: {reward}')
+            else:
+                reward = -500
+                print(f'LOST: {reward}')
+            return self.known_grid, reward, done, info
+
 
     def reset(self):
-        self.game.reset()
-        return self.game.known_grid
+        self.game.setup()
+        return self.known_grid
 
     def render(self):
-        print(self.game.known_grid)
-        print('----')
+        for i in range(self.game.rows):
+            for j in range(self.game.cols):
+                rect = pygame.Rect(j*cell_width, i*cell_height, cell_width, cell_height)
+                if self.game.mine_field[i][j].is_known:
+                    sprite = ScreenCell.get_sprite(self.game.mine_field[i][j].value)
+                else:
+                    sprite = SPRITE_UNKNWN
+                sprite = pygame.transform.scale(sprite, (cell_width, cell_height))
+                screen.blit(sprite, rect)
+        pygame.display.flip()
 
 
-class DummyGame:
-    def __init__(self):
-        self.rows = 4
-        self.cols = 2
-
-        self.grid = [
-            [1, 2],
-            [3, 4],
-            [5, 6], 
-            [7, 8]
-        ]
-
-        self.known_grid = [
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [0, 0]
-        ]
-    
-    def reveal(self, i, j):
-        self.known_grid[i][j] = self.grid[i][j]
-    
-    def reset(self):
-        self.known_grid = [
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [0, 0]
-        ]
+if __name__ == '__main__':
+    game = HardGame()
 
 
-game = DummyGame()
-env = MinesweeperEnv(game)
-# print(env.action_space.sample())
-# print(env.observation_space.sample())
+    pygame.init()
+    screen_height = 500
+    screen_width = screen_height * 1.2
+    screen = pygame.display.set_mode([screen_width, screen_height])
 
-# for i in range(20):
-observation = env.reset()
-for t in range(100):
-    env.render()
-    # print(observation)
-    action = env.action_space.sample()
-    # print(action)
-    observation, reward, done, info = env.step(action)
-    if done:
-        print(f'Episode finished after {t+1} timesteps')
-        break
-env.close()
+    cell_width = screen_width // game.cols
+    cell_height = screen_height // game.rows
+
+    screen.fill((255, 255, 255))
 
 
+    env = MinesweeperEnv(game)
+    # print(env.action_space.sample())
+    # print(env.observation_space.sample())
 
-print('done')
+    for i in range(200):
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            # print(observation)
+            action = env.action_space.sample()
+            # print(action)
+            observation, reward, done, info = env.step(action)
+            if done:
+                print(f'Episode finished after {t+1} timesteps')
+                break
+            sleep(0.1)
+    env.close()
+
+    pygame.quit()
+
+
+    print('done')
